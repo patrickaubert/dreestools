4de153afb8a49fc0b3184037775929897d97ae03
patrickaubert <patrick.aubert@m4x.org>
2021-02-06 19:55:38 +0100

    Ajoute list_datadrees

---
+++ R/liste_datadrees.R
@@ -0,0 +1,17 @@
+#' Récupère la liste de tous les fichiers data.drees
+#'
+#' La fonction retourne un tableau contenant le nom et les métadonnées de tous les fichiers data.drees
+#' diffusés par la DREES. Ces fichiers sont plus nombreux que les jeux de données, car un même
+#' jeu de données peut contenir plusieurs fichiers (.xlsx, .csv, .zip, etc.)
+#'
+#' Si une liste de rubriques est indiquée, seuls les fichiers de ces rubriques seront retenus.
+#'
+#' @param rubriques une liste de rubriques (par défaut toutes les rubriques disponibles)
+#'
+#' @return
+#' @export
+#'
+#' @examples liste_datadrees(rubriques = c("Retraite"))
+liste_datadrees <- function(rubriques = NA) {
+
+}
